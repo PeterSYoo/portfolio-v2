680ced5576d04551a9c4c7f71bf2f91f706d768b
Peter Yoo <trustintofu@gmail.com>
2022-07-27 18:32:36 -0700

    wip: adds to footer component, media queries for tablet and mobile sizes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,54 @@
 import './footer.scss';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Peter Yoo
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Experience</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/petersyoo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/PeterSYoo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Peter Yoo. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
