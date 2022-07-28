3c06fca83aeb9285944de982e530ff1060c7e170
Peter Yoo <trustintofu@gmail.com>
2022-07-27 18:09:48 -0700

    wip: adds to contact component, media queries for tablet and mobile sizes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,75 @@
 import './contact.scss';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import { MdOutlineMail } from 'react-icons/md';
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_q7620ov',
+        'template_g0tmeje',
+        form.current,
+        '4SRkwz54ROm3Wq0x-'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>petersyoox@gmail.com</h5>
+            <a
+              href="mailto:petersyoox@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
