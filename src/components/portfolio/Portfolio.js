d1a8a4654fc1ab7727f7f076553a6a7ec8ecd1f4
Peter Yoo <trustintofu@gmail.com>
2022-07-29 13:42:38 -0700

    wip: adds onscreen animation of portfolio cards when user scrolls cards into view

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -6,6 +6,36 @@ import GREETR from '../../assets/images/greetr-thumb.png';
 import SKATEREACT from '../../assets/images/skatereact-thumb.png';
 import TTEOKBOWL from '../../assets/images/tteokbowl-thumb.png';
 import RANDOMLIT from '../../assets/images/randomlit-thumb.png';
+import { motion, Variants } from 'framer-motion';
+
+const imageAnimate = {
+  offScreen: { x: -100, opacity: 0 },
+  onScreen: {
+    x: 0,
+    opacity: 1,
+    rotate: [0, 10, 0],
+    transition: { type: 'spring', bounce: 0.4, duration: 1 },
+  },
+};
+
+const btnAnimate = {
+  offScreen: { x: -100, opacity: 0 },
+  onScreen: {
+    x: 0,
+    opacity: 1,
+    rotate: [0, 10, 0],
+    transition: { type: 'spring', bounce: 0.4, duration: -2 },
+  },
+};
+
+const textAnimate = {
+  offScreen: { y: 100, opacity: 0 },
+  onScreen: {
+    y: 0,
+    opacity: 1,
+    transition: { type: 'spring', bounce: 0.4, duration: 1 },
+  },
+};
 
 const data = [
   {
@@ -68,32 +98,44 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         {data.map(({ id, image, title, github, demo }) => {
           return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
+            <motion.article
+              key={id}
+              className="portfolio__item"
+              initial={'offScreen'}
+              whileInView={'onScreen'}
+              viewport={{ once: false, amount: 0.2 }}
+              transition={{ staggerChildren: 0.1 }}
+            >
+              <motion.div
+                className="portfolio__item-image"
+                variants={imageAnimate}
+              >
                 <img src={image} alt={title} />
-              </div>
-              <h3>{title}</h3>
+              </motion.div>
+              <motion.h3 variants={textAnimate}>{title}</motion.h3>
               <div className="portfolio__item-cta">
-                <a
+                <motion.a
                   href={github}
                   className="btn"
                   target="_blank"
                   rel="noreferrer"
+                  variants={btnAnimate}
                 >
                   Github
-                </a>
+                </motion.a>
                 {demo ? (
-                  <a
+                  <motion.a
                     href={demo}
                     className="btn btn-primary"
                     target="_blank"
                     rel="noreferrer"
+                    variants={btnAnimate}
                   >
                     Live Demo
-                  </a>
+                  </motion.a>
                 ) : null}
               </div>
-            </article>
+            </motion.article>
           );
         })}
       </div>
