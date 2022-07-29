c6010a4410c8135a372a4c34667a91c11980f045
Peter Yoo <trustintofu@gmail.com>
2022-07-29 11:58:26 -0700

    wip: updates resume link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -29,7 +29,11 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="#" target="_blank" rel="norefererr">
+        <a
+          href="https://docs.google.com/document/d/e/2PACX-1vStMRPgw3QKH7q59KYge2Rga2g8rEp4hPZ_kH2TUBjuY9W5CANQ3kGCqFf__U1sTMbbd5yCBMGqVW9_/pub"
+          target="_blank"
+          rel="noreferrer"
+        >
           <motion.button
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -3,7 +3,11 @@ import { motion } from 'framer-motion';
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="#" target="_blank" rel="norefererr">
+      <a
+        href="https://docs.google.com/document/d/e/2PACX-1vStMRPgw3QKH7q59KYge2Rga2g8rEp4hPZ_kH2TUBjuY9W5CANQ3kGCqFf__U1sTMbbd5yCBMGqVW9_/pub"
+        target="_blank"
+        rel="noreferrer"
+      >
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
