19ce8d73baf9d42bf8e2adaf2cd5029a1adf79dd
Peter Yoo <trustintofu@gmail.com>
2022-07-31 12:27:43 -0700

    wip: updates toolkit icons

--- src/components/toolkit/Toolkit.js
+++ src/components/toolkit/Toolkit.js
@@ -1,5 +1,17 @@
 import './toolkit.scss';
 import { CgCheckR } from 'react-icons/cg';
+import { SiHtml5 } from 'react-icons/si';
+import { SiCss3 } from 'react-icons/si';
+import { SiJavascript } from 'react-icons/si';
+import { SiTypescript } from 'react-icons/si';
+import { SiJquery } from 'react-icons/si';
+import { SiReact } from 'react-icons/si';
+import { SiPython } from 'react-icons/si';
+import { SiMongodb } from 'react-icons/si';
+import { SiExpress } from 'react-icons/si';
+import { SiNodedotjs } from 'react-icons/si';
+import { SiDjango } from 'react-icons/si';
+import { SiPostgresql } from 'react-icons/si';
 import { motion } from 'framer-motion';
 
 const imageAnimate = {
@@ -30,6 +42,86 @@ const textAnimate = {
   },
 };
 
+const text2Animate = {
+  offScreen: { x: -300, opacity: 0 },
+  onScreen: {
+    x: 0,
+    opacity: 1,
+    transition: { type: 'spring', bounce: 0.4, duration: 1 },
+  },
+};
+
+const frontEndData = [
+  {
+    id: 1,
+    icon: <SiHtml5 className="toolkit__details-icon" />,
+    name: 'HTML',
+  },
+  {
+    id: 2,
+    icon: <SiCss3 className="toolkit__details-icon" />,
+    name: 'CSS',
+  },
+  {
+    id: 3,
+    icon: <SiJavascript className="toolkit__details-icon" />,
+    name: 'JavaScript',
+  },
+  {
+    id: 4,
+    icon: <SiTypescript className="toolkit__details-icon" />,
+    name: 'TypeScript',
+  },
+  {
+    id: 5,
+    icon: <SiJquery className="toolkit__details-icon" />,
+    name: 'jQuery',
+  },
+  {
+    id: 6,
+    icon: <SiReact className="toolkit__details-icon" />,
+    name: 'React',
+  },
+];
+
+const backEndData = [
+  {
+    id: 1,
+    icon: <SiPython className="toolkit__details-icon" />,
+    name: 'Python',
+  },
+  {
+    id: 2,
+    icon: <SiMongodb className="toolkit__details-icon" />,
+    name: 'MongoDB',
+  },
+  {
+    id: 3,
+    icon: <CgCheckR className="toolkit__details-icon" />,
+    name: 'Mongoose',
+  },
+  {
+    id: 4,
+    icon: <SiExpress className="toolkit__details-icon" />,
+    name: 'ExpressJS',
+  },
+  {
+    id: 5,
+    icon: <SiNodedotjs className="toolkit__details-icon" />,
+    name: 'NodeJS',
+  },
+  {
+    id: 6,
+    icon: <SiDjango className="toolkit__details-icon" />,
+    name: 'Django',
+  },
+  {
+    id: 7,
+    icon: <SiPostgresql className="toolkit__details-icon" />,
+    name: 'PostgreSQL',
+  },
+];
+
 const Toolkit = () => {
   return (
     <motion.section
@@ -42,95 +134,40 @@ const Toolkit = () => {
       <motion.h5 variants={textAnimate}>What Skills I Have</motion.h5>
       <motion.h2 variants={textAnimate}>My toolkit</motion.h2>
       <div className="container toolkit__container">
+        {/* START OF FRONTEND */}
         <motion.div className="toolkit__frontend" variants={btnAnimate}>
           <motion.h3 variants={textAnimate}>Frontend Development</motion.h3>
           <motion.div className="toolkit__content" variants={btnAnimate}>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>HTML</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>CSS</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>TypeScript</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>jQuery</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>React</h4>
-              </div>
-            </article>
+            {frontEndData.map(({ id, icon, name }) => {
+              return (
+                <article key={id} className="toolkit__details">
+                  <motion.div variants={textAnimate}>{icon}</motion.div>
+                  <div>
+                    <motion.h4 variants={text2Animate}>{name}</motion.h4>
+                  </div>
+                </article>
+              );
+            })}
           </motion.div>
         </motion.div>
         {/* END OF FRONTEND */}
+        {/* START OF BACKEND */}
         <motion.div className="toolkit__backend" variants={btnAnimate}>
           <motion.h3 variants={textAnimate}>Backend Development</motion.h3>
           <motion.div className="toolkit__content" variants={btnAnimate}>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>NodeJS</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>MongoDB</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>Mongoose</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>Django</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>PostgreSQL</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>Python</h4>
-              </div>
-            </article>
-            <article className="toolkit__details">
-              <CgCheckR className="toolkit__details-icon" />
-              <div>
-                <h4>ExpressJS</h4>
-              </div>
-            </article>
+            {backEndData.map(({ id, icon, name }) => {
+              return (
+                <article key={id} className="toolkit__details">
+                  <motion.div variants={textAnimate}>{icon}</motion.div>
+                  <div>
+                    <motion.h4 variants={text2Animate}>{name}</motion.h4>
+                  </div>
+                </article>
+              );
+            })}
           </motion.div>
         </motion.div>
+        {/* END OF BACKEND */}
       </div>
     </motion.section>
   );
